6b2ebc2f2433d917fc40368d8133df17789ba853
muffafa <mm.savar@gmail.com>
2023-05-18 04:43:32 +0300

    feat: example level maked

--- src/app/levels/vrf/page.tsx
+++ src/app/levels/vrf/page.tsx
@@ -1,53 +1,57 @@
 import React from "react";
 import Level from "@/components/Levels/Level";
-import vrfPhoto from "public/assets/images/nfts/vrf.jpeg";
+import vrfPhoto from "public/assets/images/nfts/vrf.jpg";
 
 function vrf() {
   return (
     <Level
       nftPhoto={vrfPhoto}
-      levelName={"vrf"}
+      levelName={"VRF"}
       date={"13/05/23"}
       levelAddress={"vrf"}
     >
       <h1>
-        <b>Level ismi</b>
+        <b>Level Name</b>
       </h1>
       <br />
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut dolorum eius
-        labore perspiciatis modi perferendis, aperiam nesciunt veniam eos!
-        Accusamus, itaque. Quia animi ab minima a odio cupiditate quam sit!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores a
+        autem enim. Ipsum tenetur error, ut molestiae tempore necessitatibus
+        sunt in voluptates deleniti ipsa neque voluptas officiis repudiandae
+        accusamus. Inventore! Quas aspernatur error at obcaecati dolores quaerat
+        facilis, possimus optio voluptas dicta mollitia nemo quibusdam
+        consequuntur architecto necessitatibus perspiciatis, aliquid ut quis
+        nihil fugit! Ratione cumque maiores numquam animi. Maiores? Provident
+        magni nam iusto! Cum sit numquam, asperiores cupiditate, qui odit
+        molestias veniam ducimus vitae ipsa magnam officiis ea non soluta
+        aperiam, quaerat placeat maiores minus dignissimos iste culpa assumenda!
+        Et consequatur molestias debitis eligendi reiciendis
       </p>
       <br />
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut dolorum eius
-        labore perspiciatis modi perferendis, aperiam nesciunt veniam eos!
-        Accusamus, itaque. Quia animi ab minima a odio cupiditate quam sit!
-      </p>
-      <br />
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut dolorum eius
-        labore perspiciatis modi perferendis, aperiam nesciunt veniam eos!
-        Accusamus, itaque. Quia animi ab minima a odio cupiditate quam sit!
-      </p>
-      <br />
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut dolorum eius
-        labore perspiciatis modi perferendis, aperiam nesciunt veniam eos!
-        Accusamus, itaque. Quia animi ab minima a odio cupiditate quam sit!
-      </p>
-      <br />
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut dolorum eius
-        labore perspiciatis modi perferendis, aperiam nesciunt veniam eos!
-        Accusamus, itaque. Quia animi ab minima a odio cupiditate quam sit!
-      </p>
-      <br />
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut dolorum eius
-        labore perspiciatis modi perferendis, aperiam nesciunt veniam eos!
-        Accusamus, itaque. Quia animi ab minima a odio cupiditate quam sit!
+        dolores similique nobis, veniam perferendis dolorem, blanditiis sed,
+        molestiae consequuntur rem nam ad maxime? Cum odit magni, non atque
+        aspernatur quae minima alias corporis? Non quia, ullam velit eos quae
+        nam nihil veritatis accusantium quo assumenda voluptatem iste
+        perferendis, fuga quis veniam vero. Aliquid eaque libero eos rerum
+        temporibus repellat veniam nemo nam explicabo! Iure dolorum laboriosam
+        aliquid fugit vero, fugiat modi. Distinctio consequuntur ipsam sunt
+        obcaecati vitae et eveniet praesentium. Quas quod numquam, blanditiis
+        molestias ipsum voluptates natus ex, adipisci saepe eveniet optio?
+        Corporis at odio ab quae voluptate porro quidem, culpa tempora eos
+        reiciendis eligendi nisi, rem maiores quod. Ducimus amet enim porro
+        incidunt excepturi, nostrum iure dolor impedit, totam, accusantium
+        saepe! Minima doloribus amet, optio consequatur a exercitationem ipsa
+        natus maxime delectus. Tenetur similique magnam pariatur ducimus
+        obcaecati commodi excepturi ipsam assumenda nemo id, repellendus amet?
+        Ut error obcaecati vitae vero! Rerum corrupti optio, ex ducimus eos
+        officiis corporis sunt officia quas? Ducimus cumque recusandae
+        architecto aliquam! Itaque doloremque quaerat eaque consequuntur
+        incidunt, voluptatem provident, distinctio laborum non consectetur sequi
+        voluptate. Expedita perspiciatis facilis, distinctio culpa quidem
+        provident. Libero molestiae inventore magni cum voluptatem, deserunt
+        aliquid laboriosam officiis atque nihil sint suscipit a id
+        necessitatibus debitis assumenda unde quaerat similique at?
       </p>
     </Level>
   );
