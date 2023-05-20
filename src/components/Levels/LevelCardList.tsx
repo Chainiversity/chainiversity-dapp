fbf6e6e25ada3590ba10d61838db6337f5e9777c
muffafa <mm.savar@gmail.com>
2023-05-20 22:07:54 +0300

    style: added margin to frontend levels card

--- src/components/Levels/LevelCardList.tsx
+++ src/components/Levels/LevelCardList.tsx
@@ -6,7 +6,7 @@ import automation from "public/assets/images/nfts/automation.jpg";
 function LevelCardList() {
   return (
     <div className="flex flex-wrap mt-10 -mx-1 lg:-mx-4 w-full">
-      <div className="w-full md:w-1/2 lg:w-1/3">
+      <div className="w-full md:w-1/2 lg:w-1/3 mb-10 lg:mb-0">
         <LevelCard
           levelAddress="vrf"
           levelName="VRF"
@@ -14,7 +14,7 @@ function LevelCardList() {
           nftPhoto={vrf}
         />
       </div>
-      <div className="w-full md:w-1/2 lg:w-1/3">
+      <div className="w-full md:w-1/2 lg:w-1/3 mb-10 lg:mb-0">
         <LevelCard
           levelAddress="automation"
           levelName="AUTOMATION"
@@ -22,7 +22,7 @@ function LevelCardList() {
           nftPhoto={automation}
         />
       </div>
-      <div className="w-full md:w-1/2 lg:w-1/3">
+      <div className="w-full md:w-1/2 lg:w-1/3 mb-10 lg:mb-0 ">
         <LevelCard
           levelAddress="priceFeed"
           levelName="PRICE FEED"
