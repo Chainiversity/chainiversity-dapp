06c39726b4b4330d3a21f64c2d4ba036e5a59632
muffafa <mm.savar@gmail.com>
2023-05-21 10:27:34 +0300

    feat: homepage vrf text updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,27 +15,19 @@ export default function Home() {
 
       <figure className="mx-auto text-center my-10 p-10">
         <blockquote>
-          <p className="text-2xl italic font-medium text-gray-900">
+          <p className="text-2xl italic font-medium text-gray-900 my-5">
             &quot;Do you want to enhance your Solidity development skills? Do
             you want to learn how to integrate Chainlink services in your smart
             contracts? Do you want to certify your knowledge of Solidity and
             Chainlink? Then, Chainiversity is the right place for you!
           </p>
-          <br />
-          <p className="text-2xl italic font-medium text-gray-900">
-            Chainiversity is an interactive educational hub for developers who
-            want to excel in Chainlink and Solidity development. There are
-            levels that developers need to submit a solution for using Chainlink
-            services. There are currently three levels in Chainiversity where
-            one of Chainlink Price Feeds, Chainlink VRF and Chainlink Automation
-            is used for each level.&quot;
-          </p>
         </blockquote>
       </figure>
 
-      <h3 className="head_text text-center mb-5">LEVELS</h3>
-      <LevelCardList />
-      
+      <h3 className="text-4xl font-extrabold text-center mb-5 md:hidden">LEVELS</h3>
+      <div className="mb-10">
+        <LevelCardList />
+      </div>
     </section>
   );
 }
